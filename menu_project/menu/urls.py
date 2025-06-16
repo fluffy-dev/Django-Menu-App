9f26c7807e46d451ec7f904b61f87b0eca5bd55e
Rauan <asetovrauan95@gmail.com>
2025-06-16 22:34:24 +0500

    base for demo shows

---
+++ menu_project/menu/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+
+
+urlpatterns = [
+    path('', views.index_view, name='main_page'),
+    path('about/', views.about_view, name='about_page'),
+    path('services/', views.services_view, name='services_page'),
+    path(
+        'services/<slug:service_slug>/',
+        views.service_detail_view,
+        name='service_detail_page'
+    ),
+]
--- menu_project/menu/views.py
+++ menu_project/menu/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index_view(request):
+    """Renders the main page."""
+    return render(request, 'menu/page.html', {'title': 'Главная страница'})
+
+
+def about_view(request):
+    """Renders the 'About Us' page."""
+    return render(request, 'menu/page.html', {'title': 'О нас'})
+
+
+def services_view(request):
+    """Renders the main services page."""
+    return render(request, 'menu/page.html', {'title': 'Наши услуги'})
+
+
+def service_detail_view(request, service_slug: str):
+    """
+    Renders a specific service detail page.
+
+    Args:
+        request: The HTTP request object.
+        service_slug: The slug identifying the specific service.
+    """
+    title = f'Услуга: {service_slug.replace("-", " ").capitalize()}'
+    return render(request, 'menu/page.html', {'title': title})
