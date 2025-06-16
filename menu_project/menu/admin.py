8e7c28f1b1d9a432350697ec4d546daf858f8ed5
Rauan <asetovrauan95@gmail.com>
2025-06-16 22:27:54 +0500

    added model to admin panel

--- menu_project/menu/admin.py
+++ menu_project/menu/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import MenuItem
 
-# Register your models here.
+
+@admin.register(MenuItem)
+class MenuItemAdmin(admin.ModelAdmin):
+    """Admin configuration for the MenuItem model."""
+
+    list_display = ('name', 'menu_name', 'parent', 'url', 'named_url')
+    list_filter = ('menu_name',)
+    search_fields = ('name', 'menu_name')
+    autocomplete_fields = ('parent',)
